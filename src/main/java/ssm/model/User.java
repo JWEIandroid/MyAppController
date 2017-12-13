94312e3be6fcbe430f44a8b5fc398667b1d60309
weij <807388755@qq.com>
2017-12-13 17:36:25 +0800

    Initial commit

---
+++ src/main/java/ssm/model/User.java
@@ -0,0 +1,14 @@
+package ssm.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class User {
+
+
+    private int id;
+
+    private String name;
+
+    private String tel;
+
+}
