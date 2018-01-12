1fa65c79958e149b391355c3fa2148c755bbd5b8
weij <807388755@qq.com>
2018-01-12 21:06:08 +0800

    更新接口

--- src/main/java/ssm/model/goods.java
+++ src/main/java/ssm/model/goods.java
@@ -16,7 +16,24 @@ public class goods implements Serializable{
     private int userid;
     private User user;
     private List<String> imgurl;
+    private String create_time;
+    private String update_time;
 
+    public String getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(String create_time) {
+        this.create_time = create_time;
+    }
+
+    public String getUpdate_time() {
+        return update_time;
+    }
+
+    public void setUpdate_time(String update_time) {
+        this.update_time = update_time;
+    }
 
     public int getUserid() {
         return userid;
